564e638da80b1f703a184394787e6d0844439ce8
a22939sv <a22939@svalero.com>
2023-02-06 19:21:52 +0100

    Updated

--- app/src/main/java/com/sanvalero/recycler_view_retrofit/AdapterDatos.java
+++ app/src/main/java/com/sanvalero/recycler_view_retrofit/AdapterDatos.java
@@ -1,34 +1,51 @@
 package com.sanvalero.recycler_view_retrofit;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+
 public class AdapterDatos extends RecyclerView.Adapter<AdapterDatos.ViewHolderDatos> {
+
+    ArrayList<String> listDatos;
+
+    public AdapterDatos(ArrayList<String> listDatos) {
+        this.listDatos = listDatos;
+    }
 
     @NonNull
     @Override
     public ViewHolderDatos onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_list, null, false);
+        return new ViewHolderDatos(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolderDatos holder, int position) {
-
+        holder.asignarDatos(listDatos.get(position));
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return listDatos.size();
     }
 
     public class ViewHolderDatos extends RecyclerView.ViewHolder {
 
+        TextView dato;
+
         public ViewHolderDatos(@NonNull View itemView) {
             super(itemView);
+            dato = (TextView) itemView.findViewById(R.id.idDato);
         }
-        
+
+        public void asignarDatos(String datos) {
+            dato.setText(datos);
+        }
     }
 }
